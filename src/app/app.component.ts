ac051f0dee063202f173fdbaa1b038b2cea2b743
Nika-Prokopiv <prokopivvu@gmail.com>
2024-05-20 17:29:31 +0300

    Modify layout in App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {HomeComponent} from "./home/home.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
