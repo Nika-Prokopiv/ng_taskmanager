a0d2dcf4dbefb7ffceb496b166e268ec20ccfde8
Nika-Prokopiv <prokopivvu@gmail.com>
2024-05-25 17:35:10 +0300

    Add PageNotFound component for 404 error page

---
+++ src/app/pagenotfound/page-not-found.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>
+      <h2>404 Error</h2>
+      <span>Page Not Found</span>
+    </div>
+  `,
+  styles: ``
+})
+export class PageNotFoundComponent {
+
+}
