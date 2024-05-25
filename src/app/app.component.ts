1b5f832e80292118f20cbb9b39ec3fa1aa81ea76
Nika-Prokopiv <prokopivvu@gmail.com>
2024-05-25 17:44:30 +0300

    Enable routing (routes and router-outlet)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import {HomeComponent} from "./home/home.component";
+import {RouterOutlet} from "@angular/router";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PageNotFoundComponent} from "./pagenotfound/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home | TomatoTimer',
+    component: HomeComponent
+  },
+  {
+    path: '**', pathMatch: 'full',
+    title: 'Page not found | TomatoTimer',
+    component: PageNotFoundComponent
+  }
+];
