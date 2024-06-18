e16ba709227e6c5efba7edb1ee700fb7d230885c
Nika-Prokopiv <prokopivvu@gmail.com>
2024-06-18 14:37:01 +0300

    fix directory name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
-import {PageNotFoundComponent} from "./components/pagenotfound/page-not-found.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-  {
-    path: '',
-    title: 'Home | TomatoTimer',
-    component: HomeComponent
-  },
-  {
-    path: '**', pathMatch: 'full',
-    title: 'Page not found | TomatoTimer',
-    component: PageNotFoundComponent
-  }
+    {
+        path: '',
+        title: 'Home | TomatoTimer',
+        component: HomeComponent
+    },
+    {
+        path: '**', pathMatch: 'full',
+        title: 'Page not found | TomatoTimer',
+        component: PageNotFoundComponent
+    }
 ];
